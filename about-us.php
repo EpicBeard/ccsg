5911e51b9606bfbb96c212a0af9a0cfc4910219c
Bearded One <tommygordon103@gmail.com>
2016-09-11 23:34:49 -0500

    table anchor
    made table selections anchored to it's respective location on the page

--- about-us.php
+++ about-us.php
@@ -57,40 +57,43 @@
 <div class="container">
     <table class="about-table">
         <tr>
-            <td>Eugene Atherton</td>
+            <td><a href="#eugene">Eugene Atherton</a></td>
         </tr>
 
         <tr>
-            <td>Andjela Jurisic</td>
+            <td><a href="#andjela">Andjela Jurisic</a></td>
         </tr>
 
         <tr>
-            <td>Fernando Tresplacios</td>
+            <td><a href="#fernando">Fernando Tresplacios</a></td>
         </tr>
 
         <tr>
-            <td>Cathy Slack</td>
+            <td><a href="#cathy">Cathy Slack</a></td>
         </tr>
     </table>
 
     <table class="about-table-right">
         <tr>
-            <td>James Upchurch</td>
+            <td><a href="#james">James Upchurch</a></td>
         </tr>
 
         <tr>
-            <td>Larry Reid</td>
+            <td><a href="#larry">Larry Reid</a></td>
         </tr>
 
         <tr>
-            <td>Mike Rulo</td>
+            <td><a href="#mike">Mike Rulo</a></td>
         </tr>
 
         <tr>
-            <td>Gerald M. Gasko</td>
+            <td><a href="#gerald">Gerald M. Gasko</a>
+            </td>
         </tr>
     </table>
 </div>
+
+<a name="eugene"></a>
 
 <div class="container">
     <h2 class="person-h2">Eugene Atherton</h2>
@@ -111,10 +114,12 @@
         </div>
 </div>
 
+<a name="andjela"></a>
+
 <div class="container">
     <h2 class="person-h2">Andjela Jurisic</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Andjela Jurisic is CCSG’s expert on managing terrorist inmates in prison,
             and addressing violence and sexual assault in detention. She has 20 years of experience in capacity building
             & mentoring of diverse stakeholders in the justice and corrections sector. Her work with international organizations,
@@ -124,7 +129,7 @@
             re-integration of combatants.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Her extensive expertise of working in hostile environments, characterized by insurgencies, political turmoil,
             civil unrest and fragile states has given her far-reaching insights into radicalization, extremism and the
             individual’s role in the organization (i.e. foot soldiers vs leaders). For example, her assignment in Sri Lanka
@@ -132,13 +137,13 @@
             civilian society through risk assessments of extremist inmates along with community-led rehabilitation options.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Focusing on the Rome Memorandum on Good Practices for Rehabilitation and Reintegration of Violent Extremist
             Offenders (Global Counterterrorism Forum), she has advised governments on security sector reform (SSR) and
             the integrated management of insurgent and terrorist inmates in transitional settings.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             In her current position as Justice Advisor in Afghanistan, she is in charge of Victim and Witness Assistance
             focusing on the implementation of the Law on the Elimination of Violence Against Women (LEVAW). She works with
             key justice sector stakeholders to improve and coordinate response mechanisms to sexual and gender-based violence;
@@ -149,10 +154,12 @@
     </div>
 </div>
 
+<a name="fernando"></a>
+
 <div class="container">
     <h2 class="person-h2">Fernando Tresplacios</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Fernando Tresplacios is an accomplished executive manager with over 28 years of combined state & military
             service, with documented success in progressively responsible and challenging positions. Of the 28 years,
             20 years have been in corrections of which 10 years were at the warden level. He is currently serving in
@@ -168,77 +175,87 @@
     </div>
 </div>
 
+<a name="cathy"></a>
+
 <div class="container">
     <h2 class="person-h2">Cathy Slack</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Cathy is retired from the Colorado Department of Corrections, where she last served as an Associate Warden at Colorado State Penitentiary. Cathy worked for the Colorado Department of Corrections for 30 years in various administrative positions at several facilities with both male and female offenders. Cathy was also the Statewide Accreditation Manager.She was selected as one of 40 people nationally to attend the first PREA auditor training of the DOJ and was certified August 20, 2013 as an auditor for the PREA standards. Cathy has conducted several PREA audits of jails and prisons (state, private and federal) to date.
         </p><br>
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Cathy received the Correctional Accreditation Manager’s Association Executive Board Award in 2012. Throughout her career, Cathy has been an active participant in the American Correctional Association and has served two years on the Field Advisory Committee, two years on the Committee on International Relations and was just reappointed for another two years in 2015 to the Committee on International Relations. Cathy has been a presenter of several workshops at the American Correctional Association. Cathy has international experienced as she worked 18 months as a contractor for the State Department as Accreditation Advisor for the U.S. Embassy in Mexico City, Mexico. Cathy is a certified auditor with the American Correctional Association and has been auditing since 1994. Cathy owns CAS Consulting, Inc.
         </p>
     </div>
 </div>
 
+<a name="james"></a>
+
 <div class="container">
     <h2 class="person-h2">James Upchurch</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             James R. Upchurch began his corrections career in 1968 working evenings and weekends as a correctional officer at the Mississippi State Penitentiary while attending college. He attained a Master of Science Degree from Delta State University in Cleveland, Mississippi in 1975 and went on to complete the course work requirements for an additional graduate degree in sociology. Mr. Upchurch has since completed numerous professional development courses in corrections and management.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Mr. Upchurch continued his corrections career in the Mississippi Department of Corrections promoting to Deputy Warden in 1979. In 1982, he accepted a position with the Arizona Department of Corrections as Warden of the Santa Cruz Unit of the Arizona State Prison Complex - Perryville. He served as the Unit Warden of all custody levels and types of institutions in Arizona until 1992 when he was promoted to Complex Warden at the 3,200 bed Arizona State Prison Complex - Florence. Included in the facilities managed by Mr. Upchurch are death row/administrative segregation, “super max.“, juvenile, female, reception and diagnostic, mental health/forensic, as well as maximum, medium and minimum custody.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Since 1996 Mr. Upchurch has served as the Chief of the Bureau of Security Operations for the Florida Department of Corrections. In this capacity he is responsible for establishing the security standards and procedures for the third largest corrections system in the country, including over 100,000 inmates and 17,000 uniformed staff. It is also his responsibility to manage the comprehensive security auditing program insuring security standards compliance and providing assistance and direction in standards application and implementation. Additionally, he manages security staffing allocations statewide to insure uniform and appropriate staff utilization. Mr. Upchurch coordinates and monitors the training and overall readiness of the department’s emergency response teams. He also chairs the department’s technology evaluation committee and supervises the emergency reporting system for critical incidents. He and members of his staff serve on a variety of agency committees providing security and institutional operations input in such areas as facility design and renovation projects, statute and administrative code development and legislative budget requests.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Mr. Upchurch has served as an expert witness in federal court proceedings both for the department and other jurisdictions, a security consultant and instructor for the National Institute of Corrections in addition to private consulting in several operational areas of corrections and a trainer in a variety of settings. He is a member of the National Law Enforcement and Corrections Technology Advisory Committee and the Technical Working Group for Corrections both affiliated with the National Institute of Justice of the United States Attorney General’s Office. He is a long-time member of the American Correctional Association and other professional organizations and has presented at ACA conferences and published in Corrections Today as well as other professional publications.
         </p>
     </div>
 </div>
 
+<a name="larry"></a>
+
 <div class="container">
     <h2 class="person-h2">Larry Reid</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             He is a correctional consultant for Crime Justice Institute (CJI) and other organizations, providing expert witness, consultation and training for correctional jurisdictions and other criminal justice entities. As a consultant and trainer, Larry has worked nationally and internationally with a number of jurisdictions on facility operational plans; contemporary programs for high risk and segregated populations; staffing analysis; policy and procedure development; organizational management; and correctional security programs. Larry also provides consulting services to the Criminal Justice Institute, Association of State Correctional Administrators (ASCA) and National Institute of Corrections (NIC).
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             From 2010 to 2014 Larry worked at Colorado Department of Corrections, Central Office. He had a brief assignment as Assistant Director of Prisons prior to being appointed to Deputy Director of Prisons. In his departmental executive leadership role Larry had numerous responsibilities including prison operations management oversight, effective and efficient use of resources and supervision of wardens. Larry retired from the Colorado Department of Corrections as Deputy Director of Prison Operations in January 2014 after serving 27 years. In recognition for his dedicated work to improve Corrections, Larry received numerous awards and recognition for his contributions to include, the Colorado Criminal Justice Association’s prestigious Harry Tinsley Award.
         </p>
     </div>
 </div>
 
+<a name="mike"></a>
+
 <div class="container">
     <h2 class="person-h2">Mike Rulo</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Michael L. Rulo has been a peace officer for over 34 years and is a Colorado Peace Officer Standards and Training (POST) Certified Peace Officer, Graduate of the FBI National Academy, Member Colorado Association of Chiefs of Police, Certified Police Executive and graduate of Regis University with a B.S. in Business Administration. Mr. Rulo served as Inspector General for Colorado Department of Corrections (CDOC) for over nine years and has most recently served as Interim Chief of Police for the City of Fountain Colorado. Mr. Rulo also served as Chief of Police for the City of Woodland Park, Colorado. His current endeavors include criminal justice and security consulting privately and as a member of Peak Systems Services Group LLC.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Mr. Rulo has also provided consultation and training for the National Institute of Corrections (NIC), providing training for state and local Jail and Corrections Administrators and Investigators on the subject of Sexual Assault and Staff Sexual Misconduct in Penal Institutions. He has also presented prison intelligence systems training as guest speaker for the University of Connecticut, Dr. Henry Lee Institute of Forensic Science. Mr. Rulo has authored articles published by Corrections Today on Prison Investigations and Electronic Surveillance Technology.
         </p>
     </div>
 </div>
 
+<a name="gerald"></a>
+
 <div class="container">
-    <h2 class="person-h2">Gerald M. Gasko B.S., M.A., M.S., Ltc Us Army (ret.)</h2>
+    <h2 class="person-h2">Gerald M. Gasko</h2>
     <div class="person-nophoto">
-        <p class="robot-p-project">
-            Mr. Gasko has forty years experience in correctional administration, law enforcement management, planning, security, criminal justice, logistics, project management, multi足million dollar appropriations, budgeting and operations. During his numerous assignments with the Colorado DOC, he has held positions as Director of Prisons, Director of Correctional Services and the Inspector General. His responsibilities included supervision over all the state and private prisons in Colorado and prison support operations such as, Medical, Mental Health, Offender Management, Food Service and Laundry, Construction and Facility Management, Programs, Education, V olunteer Legal Services, Transportation and monitoring of 4 private prisons in Colorado.
+        <p class="robot-p-project" id="gene">
+            Mr. Gasko B.S., M.A., M.S., Ltc Us Army (ret.) has forty years experience in correctional administration, law enforcement management, planning, security, criminal justice, logistics, project management, multi足million dollar appropriations, budgeting and operations. During his numerous assignments with the Colorado DOC, he has held positions as Director of Prisons, Director of Correctional Services and the Inspector General. His responsibilities included supervision over all the state and private prisons in Colorado and prison support operations such as, Medical, Mental Health, Offender Management, Food Service and Laundry, Construction and Facility Management, Programs, Education, V olunteer Legal Services, Transportation and monitoring of 4 private prisons in Colorado.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Before retiring from the United States Army in February 1990, Mr. Gasko spent 22 years in the Army, Military Police, attaining the rank of Lieutenant Colonel, and is veteran of the Vietnam War. While in the US Army, Mr. Gasko has had tours of duty overseas and in the United States in law enforcement, physical security, investigations, prison of war management, and Military Corrections. He has spent several tours of duty at the United States Disciplinary Barracks, Fr. Leavenworth, Kansas where he was the Deputy Commandant before his retirement.
         </p><br>
 
-        <p class="robot-p-project">
+        <p class="robot-p-project" id="gene">
             Mr. Gasko has continued to stay involved in the Criminal Justice field sitting as the chair of the National Law Enforcement and Corrections Technology Center, regional advisory council, volunteering as a teacher for inmates at the Colorado prisons, a volunteer case manager for a veterans group, The Home Front Cares, a volunteer at the Garden of the Gods, a city park and National Natural Landmark in Colorado Springs, an ACA auditor and acting as an expert and for consulting witness/advisor.
         </p>
     </div>
